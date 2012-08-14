1a93488f6e84675a9810a5c98e0fe9fd6bb2473a
Jan Muehlemann <jan.muehlemann@gmail.com>
2012-08-14 12:49:35 +0200

    moved en,de to main

--- client/app/main.js
+++ client/app/main.js
@@ -28,6 +28,67 @@ function(ns, $, Backbone, i18next, data, resSync) {
 
     app.store = new Backbone.Model();
 
+    app.languages = ['en', 'de'];
+    app.resStore = {
+        en: {
+            translation: {
+                layout: {
+                    header: {
+                        language: 'english'
+                    }
+                },
+                editor: {
+                    choose: 'select',
+                    addKey: 'add Key',
+                    add: 'add',
+                    'delete': 'delete',
+                    edit: 'edit',
+                    cancel: 'cancel',
+                    save: 'save',
+                    test: 'test',
+                    th: {
+                        key: 'key',
+                            specificValue: 'specific value',
+                            displayedValue: 'displayed value'
+                    },
+                    resourceItem: {
+                        options: 'options',
+                        optionsDesc: 'one option per line, eg. count=0'
+                    }
+                }
+            }
+        },
+
+        de: {
+            translation: {
+                layout: {
+                    header: {
+                        language: 'deutsch'
+                    }
+                },
+                editor: {
+                    choose: 'Wählen',
+                    addKey: 'Schlüssel hinzufügen',
+                    add: 'hinzufügen',
+                    'delete': 'löschen',
+                    edit: 'ändern',
+                    cancel: 'abbrechen',
+                    save: 'speichern',
+                    test: 'testen',
+                    th: {
+                        key: 'Schlüssel',
+                        specificValue: 'spezifischer Wert',
+                        displayedValue: 'angezeigter Wert'
+                    },
+                    resourceItem: {
+                        options: 'Optionen',
+                        optionsDesc: 'Eine Option pro Zeile, bspw. count=0'
+                    }
+                }
+            }
+        }
+    };
+
     app.setLng = function(lng) {
         lng = lng.substring(0,2);
         if (app.languages.indexOf(lng) < 0) lng = i18next.options.fallbackLng;
